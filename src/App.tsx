f4ef1693e4d3ab157f2a614e5831561bd4d38926
Marcos Ricardo <mricardoalves99@gmail.com>
2021-08-16 17:30:07 -0300

    feat: style new transaction modal

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Dashboard } from "./components/Dashboard";
 import { GlobalStyle } from "./styles/global";
 import { useState } from 'react';
 import Modal from 'react-modal';
+import { NewTransactionModal } from "./components/NewTransactionModal";
 
 Modal.setAppElement('#root');
 
@@ -23,12 +24,10 @@ export function App() {
 
       <Dashboard />
 
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
         onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+      ></NewTransactionModal>
 
       <GlobalStyle />
     </>
