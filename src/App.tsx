b470165b391fcef46f0f0afb2ecebdd2f11b794b
Marcos Ricardo <mricardoalves99@gmail.com>
2021-08-23 10:27:42 -0300

    feat: creating hook

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { GlobalStyle } from "./styles/global";
 import { useState } from 'react';
 import Modal from 'react-modal';
 import { NewTransactionModal } from "./components/NewTransactionModal";
-import { TransactionsProvider } from "./TransactionsContext";
+import { TransactionsProvider } from "./hooks/useTransactions";
 
 Modal.setAppElement('#root');
 
--- src/components/Summary/index.tsx
+++ src/components/Summary/index.tsx
@@ -2,12 +2,12 @@ import React, { useContext } from 'react';
 import incomeImg from '../../assets/income.svg';
 import outcomeImg from '../../assets/outcome.svg';
 import totalImg from '../../assets/total.svg';
-import { TransactionContext } from '../../TransactionsContext';
+import { useTransactions } from '../../hooks/useTransactions';
 
 import { Container } from './styles';
 
 export function Summary() {
-    const { transactions } = useContext(TransactionContext);
+    const { transactions } = useTransactions();
 
     const summary = transactions.reduce((acc, transaction) => {
         if (transaction.type === 'deposit') {
